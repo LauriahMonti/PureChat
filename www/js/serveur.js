d5743361999735645bec082da8467247af9fa5b4
LauriahMonti <laura.monti@epitech.eu>
2016-11-11 11:59:23 +0100

    Update serveur.js

--- www/js/serveur.js
+++ www/js/serveur.js
@@ -1,1 +1,8 @@
-// http:8080
+var http = require('http');
+var url = require("../views/connexion.html");
+http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end();
+  res.url;
+  console.log("Node.js server running on port 8080");
+}).listen(8080);
